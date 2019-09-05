d49759e7f950d636a47740b1195a1d74d2e88160
Daniel Bozinovski <BozinovskiDaniel@hotmail.com>
2019-09-05 11:58:56 +1000

    Latest

--- pcshowcase/src/components/Home.js
+++ pcshowcase/src/components/Home.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import './App.scss';
+import '../App.scss';
 import {Link} from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
+        <body>
+            <section id="wrapper" className="skewed">
+                <div className="layer bottom">
+                    <div class="content-wrap">
+                        <div className="content-body">
+                            <h1>Look Sharp</h1>
+                            <p>It is a long established fact that a reader will be distracted by the readable content
+                                of a page when looking at its layout. The point of using Lorem Ipsum is that it has
+                                a more-or-less normal distribution of letters</p>
+                        </div>
+                        <img src={require('../images/light.png')} alt="light pc" />
+                    </div>
+                </div>
 
-        </div>
+
+                <div className="layer top">
+                    <div className="content-wrap">
+                        <div className="content-body">
+                            <h1>Look Sharp</h1>
+                            <p>It is a long established fact that a reader will be distracted by the readable content
+                                of a page when looking at its layout. The point of using Lorem Ipsum is that it has
+                                a more-or-less normal distribution of letters</p>
+                        </div>
+                        <img src={require('../images/dark.png')} alt="dark pc" />
+                    </div>
+                </div>
+
+                <div className="handle"></div>
+
+
+            </section>
+
+            
+        </body>
     );
 }
 
--- pcshowcase/src/components/Nav.js
+++ pcshowcase/src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.scss';
+import '../App.scss';
 import {Link} from 'react-router-dom';
 
 function Nav() {
     return (
         <nav>
-            <h1>Tianna Rose Catania</h1>
+
         </nav>
 
     );
