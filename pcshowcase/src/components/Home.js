614fdefe819e899949f2009eb0bc168f9ab2cd03
Daniel Bozinovski <BozinovskiDaniel@hotmail.com>
2019-09-05 18:35:05 +1000

    Latest version

--- pcshowcase/src/components/Home.js
+++ pcshowcase/src/components/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import '../App.scss';
 import {Link} from 'react-router-dom';
+import '../js/main';
 
 function Home() {
     return (
         <body>
-            <section id="wrapper" className="skewed">
+
+            <section className="landing-page">
+                <div className="lpbody">
+                    <h1>Pc Showcase</h1>
+                    <p>A New Take on Hardware!</p>
+                </div>
+
+            </section>
+            
+            <section className="wrapper skewed">
                 <div className="layer bottom">
                     <div class="content-wrap">
                         <div className="content-body">
@@ -22,7 +32,7 @@ function Home() {
                 <div className="layer top">
                     <div className="content-wrap">
                         <div className="content-body">
-                            <h1>Look Sharp</h1>
+                            <h1>Stay Cool</h1>
                             <p>It is a long established fact that a reader will be distracted by the readable content
                                 of a page when looking at its layout. The point of using Lorem Ipsum is that it has
                                 a more-or-less normal distribution of letters</p>
@@ -33,7 +43,15 @@ function Home() {
 
                 <div className="handle"></div>
 
+            </section>
 
+            <section className="imageBundle">
+                <div className="grid-container">
+                    <div className="grid-item">1</div>
+                    <div className="grid-item">2</div>
+                    <div className="grid-item">3</div>
+                    <div className="grid-item">4</div>
+                </div>              
             </section>
 
             
