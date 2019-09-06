2352314ec421562a73ea67cfea74d9c662ad55d8
BozinovsiDaniel <Bozinovskidaniel@hotmail.com>
2019-09-06 10:41:48 +1000

    Newest Commit

--- pcshowcase/src/components/Nav.js
+++ pcshowcase/src/components/Nav.js
@@ -6,20 +6,20 @@ import '../js/main';
 function Nav() {
     return (
         <nav className="navbar sticky-top navbar-expand-lg">
-            <Link to="/"><a className="navbar-brand" href="#">PC Showcase</a></Link>
+            <Link to="/"><a className="navbar-brand">PC Showcase</a></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="nav navbar-nav mx-auto">
+            <ul className="nav navbar-nav ml-auto">
                 <li className="nav-item active">
-                    <Link to="/"><a className="nav-link">Home<span className="sr-only">(current)</span></a></Link>
+                    <Link to="/"><a className="nav-link">HOME<span className="sr-only">(current)</span></a></Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/store"><a className="nav-link">Store</a></Link>
+                    <Link to="/store"><a className="nav-link">STORE</a></Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/contact"><a className="nav-link">Contact Us</a></Link>
+                    <Link to="/contact"><a className="nav-link">CONTACT US</a></Link>
                 </li>
             </ul>
         </div>
